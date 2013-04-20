6356ab1a525fa71d53651af32d805a893180b60a
Nicolas McCurdy <thenickperson@gmail.com>
2013-04-20 15:29:11 -0400

    Initialize with libnotify in main()

--- centro.cpp
+++ centro.cpp
@@ -3,11 +3,11 @@
 #include <libnotifymm.h>
 
 void notify() {
-  Notify::init("Hello world!");
   Notify::Notification Hello("Hello world!", "This is an example notification.", "dialog-information");
   Hello.show();
 }
 
 int main(int argc, char* argv[]) {
+  Notify::init("Centro");
   notify();
 }
