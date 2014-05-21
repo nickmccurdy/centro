c7f8e261ea67133d11839ceca3338dbf230c153a
Nicolas McCurdy <thenickperson@gmail.com>
2014-05-21 17:06:35 -0400

    Disable the libnotify platform

--- Makefile
+++ Makefile
@@ -1,8 +1,4 @@
 CC        = clang++
-CFLAGS    = `pkg-config --cflags libnotifymm-1.0`
-LDFLAGS   = `pkg-config --libs libnotifymm-1.0`
-CONFIG    += link_pkgconfig
-PKGCONFIG += libnotify
 
 SOURCES   = $(wildcard *.cpp platforms/*.cpp services/*.cpp)
 OBJECTS   = $(patsubst %.cpp, %.o, $(SOURCES))
--- notifier.cpp
+++ notifier.cpp
@@ -1,5 +1,4 @@
 #include "notifier.h"
-#include "platforms/libnotify_platform.h"
 #include "services/example_service.h"
 
 /* Displays a notification to the user with the current Platform. */
@@ -11,6 +10,7 @@ void Notifier::Notify(std::string title, std::string description, std::string ic
  * Starts up Centro by initializing a Platform and a Service.
  */
 int main(int argc, char* argv[]) {
+  /*
   LibnotifyPlatform libnotifyPlatform;
   Platform * platform;
   platform = dynamic_cast<Platform*>(&libnotifyPlatform);
@@ -18,4 +18,5 @@ int main(int argc, char* argv[]) {
   Notifier notifier(platform);
   ExampleService service(&notifier);
   service.Start();
+  */
 }
